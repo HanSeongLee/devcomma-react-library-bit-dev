78b75440d7e1cbebe8632e6f46fc5dd8b68b4350
HanSeongLee <jkhanseong@gmail.com>
2024-02-23 22:28:30 +0900

    feat: add disable property to Container component

--- devcomma-react-library/ui/container/container.tsx
+++ devcomma-react-library/ui/container/container.tsx
@@ -3,13 +3,22 @@ import cn from 'classnames';
 import styles from './style.module.scss';
 
 export type ContainerProps = {
+    disable?: {
+        mobile?: boolean;
+        tablet?: boolean;
+        desktop?: boolean;
+    };
     className?: string;
     children?: ReactNode;
 };
 
-export const Container = ({ className, children }: ContainerProps) => {
+export const Container = ({ disable, className, children }: ContainerProps) => {
   return (
-      <div className={cn(styles.container, className)}>
+      <div className={cn(styles.container, {
+          [styles.disableMobile]: disable?.mobile,
+          [styles.disableTablet]: disable?.tablet,
+          [styles.disableDesktop]: disable?.desktop,
+      }, className)}>
         {children}
       </div>
   );
