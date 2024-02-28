bc02f6e7ef89589eb7812fa017eccb1e935ab7d2
HanSeongLee <jkhanseong@gmail.com>
2024-02-28 23:46:51 +0900

    feat: add max-width to Container component

--- devcomma-react-library/ui/container/container.tsx
+++ devcomma-react-library/ui/container/container.tsx
@@ -8,18 +8,29 @@ export type ContainerProps = {
         tablet?: boolean;
         desktop?: boolean;
     };
+    maxWidth?: {
+        mobile?: number;
+        tablet?: number;
+        desktop?: number;
+    };
     className?: string;
     children?: ReactNode;
 };
 
-export const Container = ({ disable, className, children }: ContainerProps) => {
-  return (
-      <div className={cn(styles.container, {
-          [styles.disableMobile]: disable?.mobile,
-          [styles.disableTablet]: disable?.tablet,
-          [styles.disableDesktop]: disable?.desktop,
-      }, className)}>
-        {children}
-      </div>
-  );
+export const Container = ({ disable, maxWidth, className, children }: ContainerProps) => {
+    return (
+        <div className={cn(styles.container, {
+            [styles.disableMobile]: disable?.mobile,
+            [styles.disableTablet]: disable?.tablet,
+            [styles.disableDesktop]: disable?.desktop,
+        }, className)}
+             style={{
+                 '--max-width-mobile': maxWidth?.mobile ? `${maxWidth.mobile}px` : undefined,
+                 '--max-width-tablet': maxWidth?.tablet ? `${maxWidth.tablet}px` : undefined,
+                 '--max-width-desktop': maxWidth?.desktop ? `${maxWidth.desktop}px` : undefined,
+             } as React.CSSProperties}
+        >
+            {children}
+        </div>
+    );
 };
